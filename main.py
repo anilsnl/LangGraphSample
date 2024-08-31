458b7132b82cf70c2d42120dd5197e48c3250be6
anilsenel <anildursunsenel@gmail.com>
2024-08-31 19:19:25 +0300

    documentations updates

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from data_seeder.mongo_db_seeder import load_confluence_data
 from graph.graph import app
+from flask import Flask,jsonify,request
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+flask_app = Flask(__name__)
+
+
+@flask_app.route('/search', methods=['POST'])
+def search():
+    reqquestMpdy = request.get_json()
+    result = app.invoke({'question': reqquestMpdy['question']})
+    return jsonify(result)
+
+
+@flask_app.route('/load_data', methods=['GET'])
+def load_data():
+    load_confluence_data()
+    return jsonify({"message": "Data loaded"})
+
 if __name__ == '__main__':
     print('Hello World!')
-
-    while True:
-        user_prompt = input('--> ')
-        if user_prompt == 'exit':
-            break
-        elif user_prompt == 'load_data':
-            load_confluence_data()
-        else:
-            result = app.invoke({'question': user_prompt})
-            print(result)
+    flask_app.run(port=5000)
